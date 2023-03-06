ce2309fb74a20ffd9a6dd7faacd24448b5b11870
John Kang <johnkang1121@gmail.com>
2023-03-06 01:35:58 -0800

    getting the projects section in

---
+++ src/components/Projects.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ProjectItem from './ProjectItem';
+import tuneinImg from '../assets/tune-in.png'
+import susImg from '../assets/sus.png'
+import quizImg from '../assets/quiz.png'
+import noteImg from '../assets/note-taker.png'
+
+const Projects = () => {
+    return (
+        <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+            <h1>Projects</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, assumenda. </p>
+        </div>
+    )
+}
+export default Projects
