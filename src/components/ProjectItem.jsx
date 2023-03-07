29acd91443f99002670954eee58aa4ab9d2303d4
John Kang <johnkang1121@gmail.com>
2023-03-07 01:28:06 -0800

    getting the projects pictures in

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
-const ProjectItem = () => {
-    return (
-        <div>ProjectItem</div>
-    )
-}
+import React from "react";
 
-export default ProjectItem
+const ProjectItem = ({ img, title }) => {
+  return (
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl">
+      <img src={img} alt="/" />
+      <div>
+        <h3>
+            {title}
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectItem;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import ProjectItem from './ProjectItem';
-import tuneinImg from '../assets/tune-in.png'
-import susImg from '../assets/sus.png'
-import quizImg from '../assets/quiz.png'
-import noteImg from '../assets/note-taker.png'
+import React from "react";
+import ProjectItem from "./ProjectItem";
+import tuneinImg from "../assets/tune-in.jpg";
+import tuneinImg2 from "../assets/tuneinTest2.jpg";
+import susImg from "../assets/sus.jpg";
+import quizImg from "../assets/quiz.jpg";
+import noteImg from "../assets/note-taker.jpg";
 
 const Projects = () => {
-    return (
-        <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
-            <h1>Projects</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, assumenda. </p>
-        </div>
-    )
-}
-export default Projects
+  return (
+    <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="text-4xl font-bold text-center text-black">Projects</h1>
+      <p className="text-center py-8 ">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem,
+        assumenda.{" "}
+      </p>
+      <div className="grid sm:grid-cols-2 gap-12">
+        <ProjectItem img={tuneinImg2} title="TuneIn" />
+        <ProjectItem img={quizImg} title="Quiz" />
+        <ProjectItem img={susImg} title="SUS Report" />
+        <ProjectItem img={noteImg} title="Note Taker" />
+      </div>
+    </div>
+  );
+};
+export default Projects;
