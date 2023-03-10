c9b8c1b1ca0918af16659dae6a72490314e79f21
John Kang <johnkang1121@gmail.com>
2023-03-10 15:57:05 -0800

    adding more to the about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div id="about" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="py-4 text-4xl font-bold text-center text-black">
+        ABOUT ME
+      </h1>
+    </div>
+  );
+};
 
-export default About
+export default About;
