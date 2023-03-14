ed67639a6c9057ce9f22ff2edaa3202a96c63d04
John Kang <johnkang1121@gmail.com>
2023-03-13 21:45:50 -0700

    added comment

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Work from "./components/Work";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import About from "./components/About";
-
+// changes 
 function App() {
   return (
     <div>
